a41529f7bbe7bafefefba38837dee0c3a0b345a0
SureshChandraAdhikari <sureshadhikari2002@gmail.com>
2024-05-31 00:29:13 +0530

    add login end point

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcrypt"
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
 import { User } from "./userTypes";
+import { access } from "fs";
 
 const createUser = async (req : Request,res : Response,next:NextFunction) => {
 const { name , email, password} = req.body;
@@ -42,7 +43,7 @@ newUser = await UserModel.create({
 
 try {
     const token = sign({sub: newUser._id}, config.jwtSecret as string,{expiresIn:"7d"});
-    res.json({accessToken: token})
+    res.status(201).json({accessToken: token})
 }
 catch (error) {
     return next(createHttpError(500,  "Error while signing the jwt token"))
@@ -50,4 +51,36 @@ catch (error) {
 
 }
 
-export {createUser }
+
+const loginUser = async (req: Request, res: Response,next: NextFunction) => {
+    const {email, password} = req.body;
+    if(!email || !password) {
+        return next(createHttpError(400,"All fields are required"))
+    }
+
+const user = await UserModel.findOne({email})
+    try {
+        
+        if(!user) {
+    return next(createHttpError(404,"User not found"))
+}
+        
+    } catch (error) {
+    return next(createHttpError(404,"User not found"))
+    }
+try {
+     const isMatch = await bcrypt.compare(password,user.password)
+
+    if(!isMatch) {
+        return next(createHttpError(400,"Username or password is incorrect!"))
+    }
+
+        const token = sign({sub: user._id}, config.jwtSecret as string,{expiresIn:"7d"});
+        res.json({access_token: token})
+} catch (error) {
+    return next(createHttpError(400,"Error while Logging in user"))
+}
+
+}
+
+export {createUser,loginUser }
