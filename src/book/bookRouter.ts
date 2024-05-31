707dfe4d17c9d27a290844f046c02c28bbc6efc3
SureshChandraAdhikari <sureshadhikari2002@gmail.com>
2024-05-31 21:20:30 +0530

    Update book

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,8 +1,9 @@
 import  express from "express";
-import createBook from "./bookController";
+import {createBook} from "./bookController";
 import multer from "multer";
 import path from "node:path";
 import authenticate from "../middlewares/authenticate";
+import { updateBook } from "./bookController";
 
 
 
@@ -19,7 +20,7 @@ const upload = multer({
 
 bookRouter.post("/",authenticate,upload.fields([{name:'coverImage' , maxCount:1},{name:'file',maxCount:1}]), createBook);
 
-
+bookRouter.patch("/:bookId",authenticate,upload.fields([{name:'coverImage' , maxCount:1},{name:'file',maxCount:1}]), updateBook);
 
 
 
--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,37 +1,40 @@
-import { NextFunction , Request,Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
-import  { verify} from "jsonwebtoken";
+import { verify, JwtPayload } from "jsonwebtoken";
 import { config } from "../config/config";
 
 export interface AuthRequest extends Request {
-userId: string;
+  userId: string;
 }
-const authenticate = (req: Request , res: Response , next:NextFunction) => {
 
+// Define an interface for the JWT payload
+interface JwtPayloadWithSub extends JwtPayload {
+  sub: string;
+}
 
+const authenticate = (req: Request, res: Response, next: NextFunction) => {
+  const token = req.header('Authorization');
+  if (!token) {
+    return next(createHttpError(401, "Authorization token is required"));
+  }
 
-    const token = req.header('Authorization');
-    if (!token) {
-        return next(createHttpError(401, "Authorization token is required"));
-    }
-
-   
-
-try {
-     const parsedToken = token .split(' ')[1];
-    const decoded = verify(parsedToken, config.jwtSecret as string);
+  try {
+    const parsedToken = token.split(' ')[1];
+    const decoded = verify(parsedToken, config.jwtSecret as string) as JwtPayloadWithSub;
     console.log("decoded", decoded);
 
-const _req = req as AuthRequest
-_req.userId = decoded.sub as string;
+    if (!decoded || !decoded.sub) {
+      return next(createHttpError(401, "Invalid token"));
+    }
+
+    const _req = req as AuthRequest;
+    _req.userId = decoded.sub;
     next();
+  } catch (error) {
+    console.error(error);
+    return next(createHttpError(401, "Token expired or invalid"));
+  }
+};
 
-} catch (error) {
-    return next(createHttpError(401,"Token expired"));
-}
+export default authenticate;
 
-    
-}
-
-
-export default authenticate;    
