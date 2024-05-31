b319683fb1dd60b562ae6be57603bf79649c88b0
SureshChandraAdhikari <sureshadhikari2002@gmail.com>
2024-06-01 01:28:16 +0530

    add delete end point

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -150,8 +150,82 @@ const updateBook = async (req: Request, res: Response, next: NextFunction) => {
     res.json(updatedBook);
 };
 
+const listBooks = async (req: Request, res: Response, next: NextFunction) => {
+    // const sleep = await new Promise((resolve) => setTimeout(resolve, 5000));
 
+    try {
+        // todo: add pagination.
+        const book = await BookModel.find().populate("author", "name");
+        res.json(book);
+    } catch (err) {
+        return next(createHttpError(500, "Error while getting a book"));
+    }
+};
 
-export {createBook,updateBook}
+const getSingleBook = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const bookId = req.params.bookId;
+
+    try {
+        const book = await BookModel
+            .findOne({ _id: bookId })
+            // populate author field
+            .populate("author", "name");
+        if (!book) {
+            return next(createHttpError(404, "Book not found."));
+        }
+
+        return res.json(book);
+    } catch (err) {
+        return next(createHttpError(500, "Error while getting a book"));
+    }
+};
+
+const deleteBook = async (req: Request, res: Response, next: NextFunction) => {
+    const bookId = req.params.bookId;
+
+    const book = await BookModel.findOne({ _id: bookId });
+    if (!book) {
+        return next(createHttpError(404, "Book not found"));
+    }
+
+    // Check Access
+    const _req = req as AuthRequest;
+    if (book.author.toString() !== _req.userId) {
+        return next(createHttpError(403, "You can not update others book."));
+    }
+    // book-covers/dkzujeho0txi0yrfqjsm
+    // https://res.cloudinary.com/degzfrkse/image/upload/v1712590372/book-covers/u4bt9x7sv0r0cg5cuynm.png
+
+    const coverFileSplits = book.coverImage.split("/");
+    const coverImagePublicId =
+        coverFileSplits.at(-2) +
+        "/" +
+        coverFileSplits.at(-1)?.split(".").at(-2);
+
+    const bookFileSplits = book.file.split("/");
+    const bookFilePublicId =
+        bookFileSplits.at(-2) + "/" + bookFileSplits.at(-1);
+    console.log("bookFilePublicId", bookFilePublicId);
+    // todo: add try error block
+    try {
+        await cloudinary.uploader.destroy(coverImagePublicId);
+    await cloudinary.uploader.destroy(bookFilePublicId, {
+        resource_type: "raw",
+    });
+
+    await BookModel.deleteOne({ _id: bookId });
+
+    return res.sendStatus(204);
+    } catch (error) {
+        return next(createHttpError(500, "File Deletion Unsuccessfull "));
+    }
+    
+};
+
+export {createBook,updateBook,listBooks,getSingleBook,deleteBook}
 
 
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import  express from "express";
-import {createBook} from "./bookController";
+import {createBook, deleteBook, getSingleBook, listBooks} from "./bookController";
 import multer from "multer";
 import path from "node:path";
 import authenticate from "../middlewares/authenticate";
@@ -22,6 +22,11 @@ bookRouter.post("/",authenticate,upload.fields([{name:'coverImage' , maxCount:1}
 
 bookRouter.patch("/:bookId",authenticate,upload.fields([{name:'coverImage' , maxCount:1},{name:'file',maxCount:1}]), updateBook);
 
+bookRouter.get("/",listBooks);
+
+bookRouter.get("/:bookId",getSingleBook);
+
+bookRouter.delete('/:bookId',authenticate,deleteBook);
 
 
 export default bookRouter;
