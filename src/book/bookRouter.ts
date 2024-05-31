dd4395941bc00bbcf8b2edfbb81e50a34c685428
SureshChandraAdhikari <sureshadhikari2002@gmail.com>
2024-05-31 20:25:18 +0530

    add jwt authentication

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -2,6 +2,7 @@ import  express from "express";
 import createBook from "./bookController";
 import multer from "multer";
 import path from "node:path";
+import authenticate from "../middlewares/authenticate";
 
 
 
@@ -16,7 +17,7 @@ const upload = multer({
 
 
 
-bookRouter.post("/",upload.fields([{name:'coverImage' , maxCount:1},{name:'file',maxCount:1}]), createBook);
+bookRouter.post("/",authenticate,upload.fields([{name:'coverImage' , maxCount:1},{name:'file',maxCount:1}]), createBook);
 
 
 
